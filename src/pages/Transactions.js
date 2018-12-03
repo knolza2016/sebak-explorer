1bbd5a78e30969dce10fb37117a9a6022348c0a4
dahemmer <david.hemmerijckx@gmail.com>
2018-12-03 21:20:25 +0100

    feat: integrate transactions table component

--- src/pages/Transactions.js
+++ src/pages/Transactions.js
@@ -1,22 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import dateFormatter from '../util/formatters/date.formatter';
-import stringFormatter from '../util/formatters/string.formatter';
+import React, { Component } from 'react';
 import sebakService from '../sebak/service';
 import Card from '../components/Card';
-import LoadingIndicator from '../components/LoadingIndicator';
-import ActionButton from '../components/ActionButton';
-import MediaQuery from 'react-responsive';
+import TransactionsTable from '../components/TransactionsTable';
 
 class Transactions extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      transactions: {
-        data: []
-      },
-      currentPage: 0
-    }
+  state = {
+    transactions: undefined
   }
   async componentDidMount() {
     const transactions = await sebakService.getTransactions({
@@ -28,83 +17,10 @@ class Transactions extends Component {
       transactions
     });
   }
-  previousPage = () => {
-    const { transactions, currentPage } = this.state;
-    this.paginate(transactions.next(), currentPage - 1);
-  }
-  nextPage = () => {
-    const { transactions, currentPage } = this.state;
-    this.paginate(transactions.previous(), currentPage + 1);
-  }
-  paginate = (linkPromise, page) => {
-    const { currentPage } = this.state;
-
-    this.setState({
-      transactions: {
-        data: []
-      }
-    });
-
-    linkPromise.then(transactions => {
-      this.setState({
-        transactions: page < currentPage  ? this._reverseOrder(transactions) : transactions,
-        currentPage: page
-      });
-    });
-  }
-  _reverseOrder = (transactions) => {
-    transactions.data = transactions.data.reverse()
-    return transactions
-  }
   render() {
     return (
       <Card title="Transactions">
-        {
-          this.state.transactions.data.length === 0 &&
-          <LoadingIndicator/>
-        }
-        {
-          this.state.transactions.data.length > 0 &&
-          <Fragment>
-              <table className="table">
-              <tbody>
-                <MediaQuery maxWidth={768}>
-                    {this.state.transactions.data.map((transaction) => (
-                      <tr className="table__content" key={transaction.hash}>
-                        <td className="table__item">
-                            <Link to={`/transactions/${transaction.hash}`} className="link">
-                              {stringFormatter.truncate(transaction.hash, 10, '...')}
-                            </Link> confirmed {transaction.operationCount} operations on {dateFormatter.formatAsDatetime(transaction.date)}
-                        </td>
-                      </tr>
-                    ))}
-                  </MediaQuery>
-                  <MediaQuery minWidth={769}>
-                      <tr className="table__header">
-                        <th className="table__item">Hash</th>
-                        <th className="table__item" width="200px">Date</th>
-                        <th className="table__item" width="100px">Operations</th>
-                      </tr>
-                      {this.state.transactions.data.map((transaction) => (
-                        <tr className="table__content" key={transaction.hash}>
-                          <td className="table__item">
-                            <Link to={`/transactions/${transaction.hash}`} className="link">
-                              {transaction.hash}
-                            </Link>
-                          </td>
-                          <td className="table__item">{dateFormatter.formatAsDatetime(transaction.date)}</td>
-                          <td className="table__item">{transaction.operationCount}</td>
-                        </tr>
-                      ))}
-                  </MediaQuery>
-                </tbody>
-              </table>
-              <div className="paginator">
-                { this.state.currentPage > 0 && <ActionButton onClick={this.previousPage}>Previous page</ActionButton> }
-                <ActionButton onClick={this.nextPage} style={this.state.currentPage === 0 ? {marginLeft: 'auto'} : {} }>Next page</ActionButton>
-              </div>
-          </Fragment>
-        }
+        <TransactionsTable transactions={this.state.transactions}/>
       </Card>
     );
   }
